93e5e08415a3d321564ae4de95fbf817e17b3301
seher akbulut <seher@sehers-MacBook-Pro.local>
2026-08-04 13:12:35 +0300

    Introduce HCOS cognitive turn pipeline skeleton

--- lib/core/brain/cognitive_orchestrator.dart
+++ lib/core/brain/cognitive_orchestrator.dart
@@ -1,8 +1,11 @@
 import 'conversation_engine.dart';
 import 'conversation_policy.dart';
+import 'cognitive_turn_result.dart';
 import 'exit_intelligence.dart';
+import 'night_session.dart';
 import 'perception_engine.dart';
 import 'release_engine.dart';
+import 'working_mind_view.dart';
 
 /// CognitiveOrchestrator
 ///
@@ -29,4 +32,33 @@ class CognitiveOrchestrator {
     required this.conversationEngine,
     required this.exitIntelligence,
   });
+
+  CognitiveTurnResult processTurn({
+    required String message,
+    required NightSession session,
+    required WorkingMindView workingMind,
+  }) {
+    // TODO 1
+    // Perception
+
+    // TODO 2
+    // ValidatedUnderstanding
+
+    // TODO 3
+    // ReleaseEngine
+
+    // TODO 4
+    // ConversationPolicy
+
+    // TODO 5
+    // ExitIntelligence
+
+    // TODO 6
+    // ConversationEngine
+
+    // TODO 7
+    // Return CognitiveTurnResult
+
+    throw UnimplementedError();
+  }
 }
